3b3e00e59f576bd476e3e9534c414d8e5f24537a
Igor Berlenko <igor@berlenko.com>
2018-11-21 02:06:04 +0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+### btcp-node
---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  startBlock: 1,
+  blockIntervalInSeconds: 600,
+  blockElapseInterval: 1,
+  paymentAddress: '',
+  eosSettings: {
+    activePrivateKey: '',
+    httpEndpoint: ''
+  }
+}
